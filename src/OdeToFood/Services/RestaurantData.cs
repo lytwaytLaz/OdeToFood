352de53ce4081c9d69f5de42a975e0bcaacfc326
lytwaytLaz <laszlo.hago@gmail.com>
2017-02-27 16:57:18 +0100

    Create a model of displaying restaurants

---
+++ src/OdeToFood/Services/RestaurantData.cs
@@ -0,0 +1,35 @@
+﻿using OdeToFood.Models;
+using System;
+using System.Collections.Generic;
+
+namespace OdeToFood.Services
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        // we only use List temporarily before we create a DB
+        // List is not thread safe so should be avoided for webb projects
+        // that need to handle multiple users
+        List<Restaurant> _restaurants;
+
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return _restaurants;
+        }
+
+        public InMemoryRestaurantData()
+        {
+            _restaurants = new List<Restaurant>
+            {
+                new Restaurant { Id = 1, Name = "The House of Kobe" },
+                new Restaurant {Id = 2, Name = "LJ's and the Kat" },
+                new Restaurant { Id = 3, Name = "King's Contrival" }
+              
+            };
+
+        }
+    }
+}
