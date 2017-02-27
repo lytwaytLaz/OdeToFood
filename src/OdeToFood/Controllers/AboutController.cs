ec56fd90c3471f9a491579b144eba4dc3521f087
lytwaytLaz <laszlo.hago@gmail.com>
2017-02-27 07:26:07 +0100

    Add project files.

---
+++ src/OdeToFood/Controllers/AboutController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class AboutController
+    {
+        public string Phone()
+        {
+            return "+46 709 111 333";
+        }
+        public string Address()
+        {
+            return "Sweden";
+        }
+    }
+}
